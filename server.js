c066ac5d57c8579eba6d79c05ee84a19806f5d4c
Samantha Wolff <samanthawolff07@gmail.com>
2022-04-20 16:42:30 -0500

    query the database

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mysql = require('mysql2');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -7,11 +8,20 @@ const app = express();
 app.use(express.urlencoded({ extended: false}));
 app.use(express.json());
 
+// connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: '1245Sammi',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
 
-app.get('/', (req, res) => {
-    res.json({
-        message: 'Hello World!'
-    });
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
 });
 
 
