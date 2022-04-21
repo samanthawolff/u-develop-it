ffb1f80b95735127a7b6b6918cb299ff0bb540cf
Samantha Wolff <samanthawolff07@gmail.com>
2022-04-21 00:01:41 -0500

    modularize the application

---
+++ db/connection.js
@@ -0,0 +1,15 @@
+const mysql = require('mysql2');
+
+// connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: '1245Sammi',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
+
+
+module.exports = db;
